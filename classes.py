8e1fe6eb7471c389cf41b1e4ab7cfd5ce79f2329
Brian Mann <irishmann313@users.noreply.github.com>
2018-01-25 22:58:29 -0500

    God
    Dammimt

---
+++ classes.py
@@ -0,0 +1,318 @@
+# Brian Mann
+# Puerto Rico
+# Classes
+# 1/15/2018
+
+import pygame
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, pn, numPlayers, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.pn = pn
+		self.building_spaces = 12
+		self.island_spaces = 12
+		self.vpoints = 0
+		if numPlayers == 3:
+			self.doubloons = 2
+		elif numPlayers == 4:
+			self.doubloons = 3
+		else:
+			self.doubloons = 4
+
+class RoleCard(pygame.sprite.Sprite):
+	def __init__(self, role, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.role = role
+		self.image = pygame.image.load("assets\\"+role+".png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width/2, self.gs.height/2)
+		self.doubloons = 0
+
+class CargoShip(pygame.sprite.Sprite):
+	def __init__(self, numSpaces, size, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.total_Spaces = numSpaces
+		self.size = size
+		self.imagename = "assets\\cargoship" + str(self.total_Spaces) + ".png"
+		self.image = pygame.image.load(self.imagename)
+		self.rect = self.image.get_rect()
+		if (self.size == 1):
+			self.rect.center = ((61/80)*self.gs.width, (1/14)*self.gs.height)
+		elif (self.size == 2):
+			self.rect.center = ((61/80)*self.gs.width, (3/14)*self.gs.height)
+		elif (self.size == 3):
+			self.rect.center = ((61/80)*self.gs.width, (5/14)*self.gs.height)
+		self.spaces_Left = numSpaces
+
+class ColonistShip(pygame.sprite.Sprite):
+	def __init__(self, numColonists, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\colonist_ship.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((19/80)*self.gs.width, (3/14)*self.gs.height)
+		self.numColonists = numColonists
+
+class TradingHouse(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.space1 = None
+		self.space2 = None
+		self.space3 = None
+		self.space4 = None
+		self.image = pygame.image.load("assets\\trading_house.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((19/80)*self.gs.width, (1/14)*self.gs.height)
+
+class BuildingBoard(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\building_board.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width/2, 3*self.gs.height/10)
+
+class Menu(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+		self.gs = gs
+		self.image = pygame.image.load("assets\\menu.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width/2, self.gs.height/2)
+		self.numPlayers = self.gs.minPlayers
+		self.isMenu = True
+
+		self.l2 = "Play with " + str(self.numPlayers) + " players (Press the Up or Down arrows to change)"
+		self.l3 = "Press Enter to begin!"
+
+	def tick(self):
+		self.l2 = "Play with " + str(self.numPlayers) + " players (Press the Up or Down arrows to change)"
+
+	def changePlayers(self, code):
+		if code == pygame.K_UP:
+			if self.numPlayers == self.gs.maxPlayers:
+				pass
+			else:
+				self.numPlayers += 1
+		else:
+			if self.numPlayers == self.gs.minPlayers:
+				pass
+			else:
+				self.numPlayers -= 1
+
+class SmallIndigoPlant(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\small_indigo_plant.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((99/320)*self.gs.width, (11/112)*self.gs.height)
+
+class SmallSugarMill(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\small_sugar_mill.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((99/320)*self.gs.width, (21/112)*self.gs.height)
+
+class SmallMarket(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\small_market.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((99/320)*self.gs.width, (31/112)*self.gs.height)
+
+class Hacienda(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\hacienda.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((99/320)*self.gs.width, (41/112)*self.gs.height)
+
+class ConstructionHut(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\construction_hut.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((99/320)*self.gs.width, (51/112)*self.gs.height)
+
+class SmallWarehouse(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\small_warehouse.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((99/320)*self.gs.width, (61/112)*self.gs.height)
+
+class IndigoPlant(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\indigo_plant.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((259/640)*self.gs.width, (11/112)*self.gs.height)
+
+class SugarMill(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\sugar_mill.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((259/640)*self.gs.width, (21/112)*self.gs.height)
+
+class Hospice(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\hospice.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((259/640)*self.gs.width, (31/112)*self.gs.height)
+
+class Office(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\office.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((259/640)*self.gs.width, (41/112)*self.gs.height)
+
+class LargeMarket(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\large_market.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((259/640)*self.gs.width, (51/112)*self.gs.height)
+
+class LargeWarehouse(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\large_warehouse.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = ((259/640)*self.gs.width, (61/112)*self.gs.height)
+
+class TobaccoStorage(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\tobacco_storage.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width/2, (11/112)*self.gs.height)
+
+class CoffeeRoaster(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\coffee_roaster.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width/2, (21/112)*self.gs.height)
+
+class University(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\university.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width/2, (31/112)*self.gs.height)
+
+class Factory(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\factory.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width/2, (41/112)*self.gs.height)
+
+class Harbor(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\harbor.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width/2, (51/112)*self.gs.height)
+
+class Wharf(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\wharf.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width/2, (61/112)*self.gs.height)
+
+class GuildHall(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\guild_hall.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width, (16/112)*self.gs.height)
+
+class Residence(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\residence.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width, (31/112)*self.gs.height)
+
+class Fortress(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\fortress.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width, (46/112)*self.gs.height)
+
+class CustomsHouse(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\customs_house.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width, (61/112)*self.gs.height)
+
+class CityHall(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.gs = gs
+		self.image = pygame.image.load("assets\\city_hall.png")
+		self.rect = self.image.get_rect()
+		self.rect.center = (self.gs.width, (76/112)*self.gs.height)
---
+++ puertorico.py
@@ -0,0 +1,203 @@
+# Brian Mann
+# Puerto Rico
+# Main Executable
+# 1/15/2018
+
+import sys, math, os, random
+from classes import *
+from pygame.locals import *
+from random import sample
+
+class GameSpace:
+	def main(self):
+		#initialization
+		pygame.init()
+		pygame.key.set_repeat(1, 50)
+
+		# game constants
+		self.size = self.width, self.height = 1366, 768
+		self.black = 255, 255, 255
+
+		self.screen = pygame.display.set_mode(self.size)#, pygame.FULLSCREEN)
+		self.clock = pygame.time.Clock()
+
+		self.gameOver = False
+		self.quit = False
+
+		self.title = "Puerto Rico"
+
+		self.minPlayers = 3
+		self.maxPlayers = 5
+
+		# Menu Screen
+		self.menu = Menu(self)
+
+		while self.menu.isMenu == True:
+			self.clock.tick(60)
+
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					sys.exit()
+				elif event.type == KEYDOWN:
+					if (event.key == pygame.K_UP or event.key == pygame.K_DOWN):
+						self.menu.changePlayers(event.key)
+					elif event.key == pygame.K_q:
+						sys.exit()
+					elif event.key == pygame.K_RETURN:
+						self.menu.isMenu = False
+					else:
+						pass
+				else:
+					pass
+
+			self.menu.tick()
+
+			self.screen.fill(self.black)
+
+			self.screen.blit(self.menu.image, self.menu.rect)
+			self.screen.blit(pygame.font.SysFont('mono', 36, bold=True).render(str(self.title), True, (100, 100, 200)), ((self.width/2.5), 30))
+			self.screen.blit(pygame.font.SysFont('mono', 24, bold=True).render(str(self.menu.l2), True, (150,150,255)), ((self.width/5), 85))
+			self.screen.blit(pygame.font.SysFont('mono', 32, bold=True).render(str(self.menu.l3), True, (150,150,255)), ((self.width/2.75), (self.height-70)))
+
+			pygame.display.flip()
+
+		self.numPlayers = self.menu.numPlayers
+		self.players = []
+		for i in range(0,self.numPlayers):
+			self.players.append(Player(i, self.numPlayers, self))
+
+		self.numCorn = 10
+		self.numIndigo = 11
+		self.numSugar = 11
+		self.numTobacco = 9
+		self.numCoffee = 9
+		self.numQuarry = 8
+		self.numCornPlants = 10
+		self.numIndigoPlants = 12
+		self.numSugarPlants = 11
+		self.numTobaccoPlants = 9
+		self.numCoffeePlants = 8
+
+		self.colonistship = ColonistShip(self.numPlayers, self)
+		self.tradinghouse = TradingHouse(self)
+		self.settler = RoleCard("settler", self)
+		self.mayor = RoleCard("mayor", self)
+		self.builder = RoleCard("builder", self)
+		self.craftsman = RoleCard("craftsman", self)
+		self.trader = RoleCard("trader", self)
+		self.captain = RoleCard("captain", self)
+		self.buildingboard = BuildingBoard(self)
+		self.smallindigoplant = SmallIndigoPlant(self)
+		self.smallsugarmill = SmallSugarMill(self)
+		self.smallmarket = SmallMarket(self)
+		self.hacienda = Hacienda(self)
+		self.constructionhut = ConstructionHut(self)
+		self.smallwarehouse = SmallWarehouse(self)
+		self.indigoplant = IndigoPlant(self)
+		self.sugarmill = SugarMill(self)
+		self.hospice = Hospice(self)
+		self.office = Office(self)
+		self.largemarket = LargeMarket(self)
+		self.largewarehouse = LargeWarehouse(self)
+		self.tobaccostorage = TobaccoStorage(self)
+		self.coffeeroaster = CoffeeRoaster(self)
+		self.university = University(self)
+		self.factory = Factory(self)
+		self.harbor = Harbor(self)
+		self.wharf = Wharf(self)
+		self.guildhall = GuildHall(self)
+		self.residence = Residence(self)
+		self.fortress = Fortress(self)
+		self.customshouse = CustomsHouse(self)
+		self.cityhall = CityHall(self)
+
+		if self.numPlayers == 3:
+			self.vpoints = 75
+			self.colonists = 55
+			self.cargo1 = CargoShip(4, 1, self)
+			self.cargo2 = CargoShip(5, 2, self)
+			self.cargo3 = CargoShip(6, 3, self)
+			self.prospector1 = None
+			self.prospector2 = None
+			self.numPiles = 4
+			self.doubloons = 80
+		elif self.numPlayers == 4:
+			self.vpoints = 100
+			self.colonists = 75
+			self.cargo1 = CargoShip(5, 1, self)
+			self.cargo2 = CargoShip(6, 2, self)
+			self.cargo3 = CargoShip(7, 3, self)
+			self.prospector1 = RoleCard("prospector", self)
+			self.prospector2 = None
+			self.numPiles = 5
+			self.doubloons = 74
+		else:
+			self.vpoints = 122
+			self.colonists = 95
+			self.cargo1 = CargoShip(6, 1, self)
+			self.cargo2 = CargoShip(7, 2, self)
+			self.cargo3 = CargoShip(8, 3, self)
+			self.prospector1 = RoleCard("prospector", self)
+			self.prospector2 = RoleCard("prospector", self)
+			self.numPiles = 6
+			self.doubloons = 66
+
+		self.turnOrder(self.players)
+
+		# main game loop
+		while self.quit == False:
+			self.clock.tick(60)
+
+
+			# handle input
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					sys.exit()
+				elif event.type == KEYDOWN:
+					if event.key == pygame.K_q:
+						sys.exit()
+
+			# tick objects
+
+			# display objects
+			self.screen.fill(self.black)
+			self.screen.blit(self.buildingboard.image, self.buildingboard.rect)
+			self.screen.blit(self.tradinghouse.image, self.tradinghouse.rect)
+			self.screen.blit(self.colonistship.image, self.colonistship.rect)
+			self.screen.blit(self.cargo1.image, self.cargo1.rect)
+			self.screen.blit(self.cargo2.image, self.cargo2.rect)
+			self.screen.blit(self.cargo3.image, self.cargo3.rect)
+			self.screen.blit(self.smallindigoplant.image, self.smallindigoplant.rect)
+			self.screen.blit(self.smallsugarmill.image, self.smallsugarmill.rect)
+			self.screen.blit(self.smallmarket.image, self.smallmarket.rect)
+			self.screen.blit(self.hacienda.image, self.hacienda.rect)
+			self.screen.blit(self.constructionhut.image, self.constructionhut.rect)
+			self.screen.blit(self.smallwarehouse.image, self.smallwarehouse.rect)
+			self.screen.blit(self.indigoplant.image, self.indigoplant.rect)
+			self.screen.blit(self.sugarmill.image, self.sugarmill.rect)
+			self.screen.blit(self.hospice.image, self.hospice.rect)
+			self.screen.blit(self.office.image, self.office.rect)
+			self.screen.blit(self.largemarket.image, self.largemarket.rect)
+			self.screen.blit(self.largewarehouse.image, self.largewarehouse.rect)
+			self.screen.blit(self.tobaccostorage.image, self.tobaccostorage.rect)
+			self.screen.blit(self.coffeeroaster.image, self.coffeeroaster.rect)
+			self.screen.blit(self.university.image, self.university.rect)
+			self.screen.blit(self.factory.image, self.factory.rect)
+			self.screen.blit(self.harbor.image, self.harbor.rect)
+			self.screen.blit(self.wharf.image, self.wharf.rect)
+			self.screen.blit(self.guildhall.image, self.guildhall.rect)
+			self.screen.blit(self.residence.image, self.residence.rect)
+			self.screen.blit(self.fortress.image, self.fortress.rect)
+			self.screen.blit(self.customshouse.image, self.customshouse.rect)
+			self.screen.blit(self.cityhall.image, self.cityhall.rect)
+
+			pygame.display.flip()
+
+	def turnOrder(self, players):
+		myorder = sample(range(len(players)), k=len(players))
+		self.players = [self.players[i] for i in myorder]
+
+
+if __name__ == '__main__':
+	gs = GameSpace()
+	gs.main()
